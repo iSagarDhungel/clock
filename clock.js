f62980cb1b34875e0627f5dd67a549d283c636ca
Sagar Dhungel <mail@sagardhungel.com.np>
2017-12-10 12:01:08 +0545

    Error var. solved

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@ function setup() {
  }
 
  function draw() {
+
    background(0);
    translate(window.innerWidth/2, window.innerHeight/2);
    rotate(-90);
@@ -13,25 +14,27 @@ function setup() {
    let secs = second();
    let mils = millis();
 
+   let err = 0.01;
+
    noFill();
    strokeWeight(20);
 
    stroke(150, 255, 100);
-   let hourAngle = map(hrs % 12, 0, 12, 0, 360);
+   let hourAngle = map(hrs % 12, 0, 12, 0, 360) + err;
    arc(0, 0, 100, 100, 0, hourAngle);
    rotate(hourAngle);
    line(0, 0, 50, 0);
 
    stroke(150, 100, 255);
-   startAngle = -hourAngle
-   let minuteAngle = map(mins, 0, 60, 0, 360) + startAngle;
+   startAngle = -hourAngle;
+   let minuteAngle = map(mins, 0, 60, 0, 360) + startAngle + err;
    arc(0, 0, 150, 150, startAngle, minuteAngle);
    rotate(minuteAngle);
    line(0, 0, 75, 0);
 
    stroke(255, 100, 150);
-   startAngle = -minuteAngle-hourAngle
-   let secondAngle = map(secs, 0, 60, 0, 360) + startAngle;
+   startAngle = -minuteAngle-hourAngle;
+   let secondAngle = map(secs, 0, 60, 0, 360) + startAngle + err;
    arc(0, 0, 200, 200, startAngle, secondAngle);
    rotate(secondAngle);
    stroke(255, 100, 150);
@@ -39,7 +42,7 @@ function setup() {
  
    stroke(255, 100, 0);
    startAngle= -secondAngle-minuteAngle-hourAngle;
-   let miliAngle = map(mils, 0, 900, 0, 360) + startAngle;
+   let miliAngle = map(mils, 0, 900, 0, 360) + startAngle + err;
    arc(0, 0, 250, 250, startAngle, miliAngle);
    rotate(miliAngle);
    line(0, 0, 125, 0);
